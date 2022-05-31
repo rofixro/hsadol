5e44967205b15fb8b283573b2bdf33346e149356
calvest <736147366@qq.com>
2022-05-31 14:57:25 +0800

    :white_check_mark: 添加新的测试用例两数之和

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * 两数相加
+ * @param num1 第一个数字
+ * @param num2 第二个数字
+ * @typeParam Number
+ * @return(s) 返回两个数字相加之和
+ */
+export function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
