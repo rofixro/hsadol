d47ab2b89035a7ccc69a76db7f08d4dc2a11cf17
calvest <736147366@qq.com>
2022-06-01 09:44:06 +0800

    :lipstick: 使用 TypeScript 文件

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,10 @@
  * @return(s) 返回两个数字相加之和
  */
 export function sum(num1: number, num2: number): number {
-  return num1 + num2;
+  const a = 1;
+  if (!Array.isArray(a)) {
+    return num1 + num2 + a;
+  } else {
+    return num1 + num2 + a;
+  }
 }
