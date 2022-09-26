36db2a68ae1c0aa426ee70d2cede036cf2bd472f
ojhaywood <frankiewb9527@gmail.com>
2022-09-26 14:14:13 +0800

    style: add notes

--- src/Array/last.js
+++ src/Array/last.js
@@ -1,3 +1,16 @@
+/**
+ * Get the last element of the array
+ * @version 0.0.1
+ * @param {*[]} array - Arrays to be queried
+ * @returns {*} Returns the last element of the array
+ * @see https://ojhaywood.github.io/hsadol/function/index.html#static-function-last
+ * @example
+ * last([1, 2, 3])
+ * // 3
+ *
+ * last([])
+ * // undefined
+ */
 export function last(array) {
   return array ? array[array.length - 1] : undefined;
 }
