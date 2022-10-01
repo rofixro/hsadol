4aa49f4c387a138bcda1f256b76e90f48ad7797f
ojhaywood <frankiewb9527@gmail.com>
2022-10-01 20:43:34 +0800

    chore: update index

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import { head } from "./src/Array/head";
 import { last } from "./src/Array/last";
 import { join } from "./src/Array/join";
 import { drop } from "./src/Array/drop";
+import { tail } from "./src/Array/tail";
 import { reverse } from "./src/Array/reverse";
 import { compact } from "./src/Array/compact";
 
-export { head, last, compact, join, drop, reverse };
+export { head, last, compact, join, drop, tail, reverse };
